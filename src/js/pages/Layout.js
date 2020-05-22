51a54dce5a839bc7dda9ad63b231789bdb7c2f18
akine <weezer.and.you@gmail.com>
2020-05-22 22:59:29 +0900

    duh

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,15 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link, withRouter } from "react-router-dom"
 
-export default class Layout extends React.Component {
+class Layout extends React.Component {
+  navigate() {
+    console.log(this.props.history);
+    this.props.history.push("/");
+  }
   render() {
     return (
       <div>
         <h1>News.net</h1>
         {this.props.children}
-        <Link to="archives"><button class="btn btn-danger">archives</button></Link>
-        <Link to="settings"><button class="btn btn-success">settings</button></Link>
+        <Link to="archives" class="btn btn-danger">archives</Link>
+        <Link to="settings" class="btn btn-success">settings</Link>
+        <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
       </div>
     );
   }
 }
+
+export default withRouter(Layout);
